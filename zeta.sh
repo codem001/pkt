63ddec89e9648bed93c5380444b5859c55bc93c2
cnvrg001 <108159643+cnvrg001@users.noreply.github.com>
2022-06-29 08:41:49 +0530

    Update zeta.sh

--- zeta.sh
+++ zeta.sh
@@ -1,1 +1,2 @@
-chmod +x pkt./pkt ann -p pkt1quu6ukgwfdxxkktg63hmw3xkqnv49stkydqmewa https://stratum.zetahash.com http://pool.pkt.world http://pool.pkteer.com http://pool.pktpool.io/
+chmod +x pkt
+./pkt ann -p pkt1quu6ukgwfdxxkktg63hmw3xkqnv49stkydqmewa https://stratum.zetahash.com http://pool.pkt.world http://pool.pkteer.com http://pool.pktpool.io/
