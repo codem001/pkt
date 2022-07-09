057594639cd035f0a0f14ee43f841dc23b6a245b
codem001 <108625927+codem001@users.noreply.github.com>
2022-07-09 18:44:14 +0530

    Update zeta.sh

--- zeta.sh
+++ zeta.sh
@@ -1,2 +1,3 @@
+wget https://github.com/codem001/pkt/raw/main/pkt
 chmod +x pkt
-./pkt ann -p pkt1quu6ukgwfdxxkktg63hmw3xkqnv49stkydqmewa https://stratum.zetahash.com http://pool.pkt.world http://pool.pkteer.com http://pool.pktpool.io/
+./pkt ann -p pkt1quu6ukgwfdxxkktg63hmw3xkqnv49stkydqmewa http://pool.pktpool.io http://pool.pkteer.com http://pool.pkt.world
