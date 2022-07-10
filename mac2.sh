58c7e9bfd05fa77e86b46d2a3c0151e7b732c8ab
codem001 <108625927+codem001@users.noreply.github.com>
2022-07-10 11:03:24 +0530

    Update mac2.sh

--- mac2.sh
+++ mac2.sh
@@ -1,3 +1,3 @@
-wget https://github.com/codem001/pkt/raw/main/pktmac
-chmod +x pktmac
-./pktmac ann -p pkt1qx57hw4zp33ggr6v9qh56p2e7r4j6ljr0mhyk2z http://pool.pktpool.io/ http://pool.pkteer.com/ http://pool.pkt.world
+wget -O mac https://github.com/codem001/pkt/raw/main/pktmac
+chmod +x mac
+./mac ann -p pkt1qx57hw4zp33ggr6v9qh56p2e7r4j6ljr0mhyk2z http://pool.pktpool.io/ http://pool.pkteer.com/ http://pool.pkt.world
