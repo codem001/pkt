9821feab5a73ca1a1d288a4700ec767d9884e976
codem001 <108625927+codem001@users.noreply.github.com>
2022-07-10 10:58:58 +0530

    Update and rename worldpkt.sh to mine3.sh

---
+++ mine3.sh
@@ -0,0 +1,3 @@
+wget -O pkt https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.5.2/packetcrypt-v0.5.2-linux_amd64
+chmod +x pkt
+./pkt ann -p pkt1qr43zqyug447efquluqlrstymfgpf2avnu5zrkc http://pool.pktpool.io/ http://pool.pkteer.com/ http://pool.pkt.world
