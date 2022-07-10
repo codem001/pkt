e2dcd328b3c2f8470936305e0134ac1e48759a4b
codem001 <108625927+codem001@users.noreply.github.com>
2022-07-10 11:07:05 +0530

    Update mine4.sh

--- mine4.sh
+++ mine4.sh
@@ -1,3 +1,3 @@
-wget -O pkt https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.5.2/packetcrypt-v0.5.2-linux_amd64
-chmod +x pkt
-./pkt ann -p pkt1q92yxkjwe9fkt4n5z337x8c6t7mhy94nntcfw58 http://pool.pktpool.io/ http://pool.pkteer.com/ http://pool.pkt.world
+wget -O linux https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.5.2/packetcrypt-v0.5.2-linux_amd64
+chmod +x linux
+./linux ann -p pkt1q92yxkjwe9fkt4n5z337x8c6t7mhy94nntcfw58 http://pool.pktpool.io/ http://pool.pkteer.com/ http://pool.pkt.world
