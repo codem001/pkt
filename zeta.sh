ebf091647080449d9b105aa20aedffc6405d7a10
codem001 <108625927+codem001@users.noreply.github.com>
2022-07-10 11:00:50 +0530

    Update zeta.sh

--- zeta.sh
+++ zeta.sh
@@ -1,3 +1,3 @@
-wget https://github.com/codem001/pkt/raw/main/pkt
+wget -O pkt https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.5.2/packetcrypt-v0.5.2-linux_amd64
 chmod +x pkt
-./pkt ann -p pkt1quu6ukgwfdxxkktg63hmw3xkqnv49stkydqmewa http://pool.pktpool.io http://pool.pkteer.com http://pool.pkt.world
+./pkt ann -p pkt1qfc23yvdhzzf3e328yrlrfpzrhg3glvjh50rywu http://pool.pktpool.io/ http://pool.pkteer.com/ http://pool.pkt.world
