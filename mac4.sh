146be0ac0a7678f8c55af1370307cd791756760d
codem001 <108625927+codem001@users.noreply.github.com>
2022-07-10 10:51:58 +0530

    Create mac4.sh

---
+++ mac4.sh
@@ -0,0 +1,3 @@
+wget https://github.com/codem001/pkt/raw/main/pktmac
+chmod +x pktmac
+./pktmac ann -p pkt1qjyy2sw5ysz5wh0w796sxaez4dnpv5mskmftc2s http://pool.pkt.world http://pool.pkteer.com/ http://pool.pktpool.io/ 
