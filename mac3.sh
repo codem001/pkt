75a3cf5e9b1d9631574f1141f77db606407d9543
codem001 <108625927+codem001@users.noreply.github.com>
2022-07-10 10:54:13 +0530

    Update mac3.sh

--- mac3.sh
+++ mac3.sh
@@ -1,3 +1,3 @@
 wget https://github.com/codem001/pkt/raw/main/pktmac
 chmod +x pktmac
-./pktmac ann -p pkt1q6ure3navhqqkym2aln823qlhzrcmz5uzxhn80l http://pool.pktpool.io/ http://pool.pkt.world http://pool.pkteer.com/ 
+./pktmac ann -p pkt1q6ure3navhqqkym2aln823qlhzrcmz5uzxhn80l http://pool.pktpool.io/ http://pool.pkteer.com/ http://pool.pkt.world
