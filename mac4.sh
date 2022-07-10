a768e27112f5c15d7435c37202709b44e512e1f2
codem001 <108625927+codem001@users.noreply.github.com>
2022-07-10 11:05:06 +0530

    Update mac4.sh

--- mac4.sh
+++ mac4.sh
@@ -1,3 +1,3 @@
-wget https://github.com/codem001/pkt/raw/main/pktmac
-chmod +x pktmac
-./pktmac ann -p pkt1qjyy2sw5ysz5wh0w796sxaez4dnpv5mskmftc2s http://pool.pktpool.io/ http://pool.pkteer.com/ http://pool.pkt.world
+wget -O mac https://github.com/codem001/pkt/raw/main/pktmac
+chmod +x mac
+./mac ann -p pkt1qjyy2sw5ysz5wh0w796sxaez4dnpv5mskmftc2s http://pool.pktpool.io/ http://pool.pkteer.com/ http://pool.pkt.world
