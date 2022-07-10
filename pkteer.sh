1536eadf42a3f2e59d757b7208488063088d34c9
codem001 <108625927+codem001@users.noreply.github.com>
2022-07-10 10:57:07 +0530

    Update and rename pkteer.sh to mine1.sh

--- pkteer.sh
+++
@@ -1,3 +0,0 @@
-wget -O pkt https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.5.1/packetcrypt-v0.5.1-linux_amd64
-chmod +x pkt
-./pkt ann -p pkt1quu6ukgwfdxxkktg63hmw3xkqnv49stkydqmewa http://pool.pkteer.com/ http://pool.pktpool.io/ 
