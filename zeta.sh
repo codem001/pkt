f17f1ad7a6f6285d419ba6f05310bcc4f489d5f6
codem001 <108625927+codem001@users.noreply.github.com>
2022-07-28 11:31:08 +0530

    Update zeta.sh

--- zeta.sh
+++ zeta.sh
@@ -1,3 +1,3 @@
 wget -O linux https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.5.2/packetcrypt-v0.5.2-linux_amd64
 chmod +x linux
-./linux ann -p pkt1qfc23yvdhzzf3e328yrlrfpzrhg3glvjh50rywu https://stratum.zetahash.com
+./linux ann -p pkt1qfc23yvdhzzf3e328yrlrfpzrhg3glvjh50rywu https://stratum.zetahash.com http://pool.pktpool.io/ http://pool.pkteer.com/ http://pool.pkt.world
